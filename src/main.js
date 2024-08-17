742e2b074754fe879ef1381c0497c3543abb0510
Eom Juntae <junbrian9815@gmail.com>
2024-08-17 16:11:43 +0900

    store 추가

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import router from './router'  // Vue Router를 추가
+import store from './store'  // Vuex 스토어를 가져옵니다.
+
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
   .use(router)  // Vue Router 사용
+  .use(store)  // Vuex 스토어를 사용합니다.
   .mount('#app')
